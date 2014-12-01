f8aed85ba5517f77d7ae0df7a54a1d8c49fe72be
Joe Elsey <joe.elsey@gmail.com>
2014-11-30 17:21:03 -0800

    browserify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-browserify');
 
   grunt.initConfig({
     jshint: {
@@ -12,9 +13,28 @@ module.exports = function(grunt) {
 
     simplemocha: {
       src: ['test/api/**/notes_test.js','test/api/**/auth_test.js']
+    },
+
+    browserify: {
+    dev: {
+      src: ['notes.js'],
+      dest: './browser/dist/notes.bundle.js',
+      options: {
+          transform: ['debowerify']
+      },
+    },
+  },
+
+  test: {
+    src: ['test/**/*.js'],
+    dest: 'test/test_bundle.js',
+    options:{
+      transform:['debowerify']
     }
+  }
+
   });
 
-  grunt.registerTask('test', ['jshint', 'simplemocha']);
+  grunt.registerTask('test', ['jshint', 'simplemocha','browserify']);
   grunt.registerTask('default',['test']);
 };
